8c9592a66296dac14be3eaab6eea6c30050d6d78
eko802 <eko802@aucklanduni.ac.nz>
2019-09-09 00:02:23 +1200

    Add my_module [sayBye()]

--- my_module.java
+++ my_module.java
@@ -2,6 +2,10 @@ class Hello{
 	private static void sayHi(){
 		System.out.println("Hi");
 	}
+	
+	private static void sayBye(){
+		System.out.println("Bye");
+	}
 	public static void main(String[] args){
 		System.out.println("Hello");
 	}
