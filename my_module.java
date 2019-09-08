a9c53895f221f355f961e5617c635456e3b1a085
eko802 <eko802@aucklanduni.ac.nz>
2019-09-09 00:06:55 +1200

    Add my_module.java [sayHow()]

--- my_module.java
+++ my_module.java
@@ -6,6 +6,9 @@ class Hello{
 	private static void sayBye(){
 		System.out.println("Bye");
 	}
+	private static void sayHow(){
+		System.out.println("How are you?");
+	}
 	public static void main(String[] args){
 		System.out.println("Hello");
 	}
